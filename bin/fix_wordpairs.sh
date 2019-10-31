013b5b81f9021f6b8c7f16e17704491524ec1361
devYaoYH <yaoyiheng@gmail.com>
2019-10-30 22:30:09 -0500

    Fixed bash scripts for Unix env

--- bin/fix_wordpairs.sh
+++ bin/fix_wordpairs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-echo Word pair .csv file (prime,target) to fix:
+echo "Word pair .csv file (prime,target) to fix:"
 read word_file
-echo Reading from... $word_file
+echo "Reading from... $word_file"
 python filter_words.py $word_file
