f167663b80e6a7615b6c19bb3cf76de1b9965de8
devyaoyh <yaoyiheng@gmail.com>
2019-10-30 22:31:30 -0500

    Fixed minor print issue

--- bin/fix_wordpairs.bat
+++ bin/fix_wordpairs.bat
@@ -1,5 +1,5 @@
 @echo off
 cd ..
 set /P word_file="Word pair .csv file (prime,target) to fix: "
-echo "Reading from... " + %word_file%
+echo Reading from... %word_file%
 python filter_words.py %word_file%
